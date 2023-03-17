953ff2dd089e770bfb9c0b14a3c1e02cb3f12ed5
TaylorColon3 <stuwrkr-tcolon4@mx.uillinois.edu>
2023-03-17 20:09:25 +0000

    Dashboard last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-
 get("/" , { :controller => "currencies" , :action => "homepage" }) 
 
 get("forex" , { :controller => "currencies" , :action => "first_currency" }) 
+
+get( "/forex/:from_currency" , { :controller => "currencies" , :action => "second_currency" })
+
+get( "/forex/:from_currency/:to_currency" , { :controller => "currencies" , :action => "convert_currency" })
+
 end
